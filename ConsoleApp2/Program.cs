ebe7da610b5086f28c0c491549fe874d68155024
ssollnncce <126074818+ssollnncce@users.noreply.github.com>
2023-06-07 13:58:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using firsttask;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности:");
+            double circleRadius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите первую сторону трегольника:");
+            double triangleSide1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону трегольника:");
+            double triangleSide2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третью сторону трегольника:");
+            double triangleSide3 = Convert.ToDouble(Console.ReadLine());
+
+            double circleArea = Calculator.Circlearea(circleRadius);
+            double triangleArea = Calculator.TriangleArea(triangleSide1, triangleSide2, triangleSide3);
+
+            Console.WriteLine($"Площадь круга: {circleArea}");
+            Console.WriteLine($"Площадь треугольника: {triangleArea}");
+        }
+    }
+}
---
+++ firsttask/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace firsttask
+{
+    public static class Calculator
+    {
+        public static double Circlearea(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус не может быть меньше или равен 0");
+            }
+
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double TriangleArea(double side1, double side2, double side3)
+        {
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+            {
+                throw new ArgumentException("Сторона треугольника не может быть меньше или равно 0");
+            }
+            else if ((Math.Pow(side1, 2) == Math.Pow(side2, 2) + Math.Pow(side3, 2))
+                || (Math.Pow(side2, 2) == Math.Pow(side1, 2) + Math.Pow(side3, 2))
+                || (Math.Pow(side3, 2) == Math.Pow(side1, 2) + Math.Pow(side2, 2)))
+            {
+                double arearighttrin = 0.5 * Math.Min(side1, side2) * Math.Min(side2, side3);
+                return arearighttrin;
+            }
+            else
+            {
+                double semiperimeter = (side1 + side2 + side3) / 2;
+                return Math.Sqrt(semiperimeter * (semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3));
+            }
+
+        }
+    }
+}
